c243ad92929a35f26ff226bb353c222825d4cfa7
Deepak Kumar <deepakdagur0007@gmail.com>
2025-12-14 22:23:39 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use("/api/ai/generate-roadmap", protect, generateRoadmap);
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 //Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
--- frontend/src/utils/apiPath.js
+++ frontend/src/utils/apiPath.js
@@ -1,7 +1,7 @@
 // Get base URL from environment variable
 export const BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
-  "http://localhost:5000";
+  "http://localhost:8000";
 
 export const API_PATHS = {
   AUTH: {
